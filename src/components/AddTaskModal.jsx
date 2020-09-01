a954594bc13ca6be37b55ad905f9b5320c237a96
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-09-01 09:40:45 -0700

    testing the materializecss collapsible"

--- src/components/AddTaskModal.jsx
+++ src/components/AddTaskModal.jsx
@@ -8,41 +8,51 @@ export default class AddTaskModal extends React.Component {
     }
     console.log(this.props.boardData);
   }
-  display() {
 
+  display() {
     if (this.props.boardData === null) {
       return <p>props board data is null</p>
     } else {
       const boards = this.props.boardData.boards;
       console.log(boards);
       return (
-        <div className="card container m-4 p-4">
-          <p>{this.props.boardData.boards[0].name}</p>
-          <table>
-            <thead>
-              <tr>
-                <th>Task</th>
-                <th>Person</th>
-                <th>Status</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{boards[0].items[0].name}</td>
-                <td>{boards[0].items[0].column_values[0].text}</td>
-                <td>{boards[0].items[0].column_values[1].text}</td>
-                <td>{boards[0].items[0].column_values[2].text}</td>
-                <td></td>
-
-              </tr>
-              <tr>{boards[0].items[0].name}</tr>
-              <tr></tr>
-            </tbody>
-          </table>
-
-        </div>
-
+        <ul className="collapsible">
+          <li>
+            <div className="collapsible-header"><i className="material-icons">filter_drama</i>First</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">place</i>Second</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+          <li>
+            <div className="collapsible-header"><i className="material-icons">whatshot</i>Third</div>
+            <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          </li>
+        </ul>
+        // <div className="card container m-4 p-4">
+        //   <p>{this.props.boardData.boards[0].name}</p>
+        //   <table>
+        //     <thead>
+        //       <tr>
+        //         <th>Task</th>
+        //         <th>Person</th>
+        //         <th>Status</th>
+        //         <th>Date</th>
+        //       </tr>
+        //     </thead>
+        //     <tbody>
+        //       <tr>
+        //         <td>{boards[0].items[0].name}</td>
+        //         <td>{boards[0].items[0].column_values[0].text}</td>
+        //         <td>{boards[0].items[0].column_values[1].text}</td>
+        //         <td>{boards[0].items[0].column_values[2].text}</td>
+        //       </tr>
+        //       <tr>{boards[0].items[0].name}</tr>
+        //       <tr></tr>
+        //     </tbody>
+        //   </table>
+        // </div>
       )
     }
   }
